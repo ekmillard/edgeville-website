1a4c5b1933363f11bf4f87afe047a9dbc69e033c
Ethan Millard <emillard919@gmail.com>
2023-12-27 17:53:49 -0500

    Announcement system progress

--- app/View/Components/NoticeBar.php
+++ app/View/Components/NoticeBar.php
@@ -3,7 +3,7 @@
 namespace App\View\Components;
 
 use Illuminate\View\Component;
-use App\Models\Announcement; // Make sure to import your model
+use App\Models\Announcement;
 
 class NoticeBar extends Component
 {
